b1bb5f5877c97367764ae27e139a561df7e837e1
unknown <chibox@gmail.com>
2016-01-19 00:21:25 +0100

    initial release

---
+++ ext_emconf.php
@@ -0,0 +1,39 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "ch_is_templates".
+#
+# Auto generated 21-08-2015 14:06
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Indexed Search Engine Templates',
+	'description' => 'This extension provides HTML templates for the Indexed Search Engine to make it easier to design.',
+	'category' => 'plugin',
+	'shy' => 0,
+	'version' => '1.1.14',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'TYPO3_version' => '3.5.0-',
+	'PHP_version' => '3.0.0-',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Chi Hoang',
+	'author_email' => 'chibo@gmx.de',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+);
+
+?>
